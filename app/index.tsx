964a30417d22d53d7611fae860bde2c872920648
Nut06 <66050067@kmitl.ac.th>
2025-11-05 20:46:28 +0700

    pick from createmeeting commit

--- app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from "expo-router";
-
-export default function Index() {
-  // const isLoggedIn = false; // mock, ไว้เชื่อมกับ auth state จริงทีหลัง
-  // return isLoggedIn ? <Redirect href="/(main)/home" /> : <Redirect href="/login" />;
-  return <Redirect href="/(main)/home" />;
-}
